5ff4ab8a789ceea489835dcfbe607c5f74c079fe
Padde <doo@hotmail.de>
2018-09-27 11:14:13 -0400

    Added comment explaining why

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // JS is stupid, beware of type conversion errors
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
